1cccf68da7b76035b149ea15384f6adb57ea61ac
Shyla Clark <sclark6@uno.edu>
2014-02-25 13:08:50 -0600

    exercise one

--- Labs/lab5/ExerciseOne.java
+++ Labs/lab5/ExerciseOne.java
@@ -8,18 +8,8 @@ import java.util.Scanner; // create and initialize scanner
 
 public class ExerciseOne {
 	
-	public static float fToC( float t ) {
-		float celsius = (( t - 32 ) * ( 5 / 9 ));
-		return celsius;
-	} // end method fToC
-	
-	public static float cToF( float t ) {
-		float fahrenheit = (( t + 32 ) * ( 9 / 5 ));
-		return fahrenheit;
-	} // end method cToF
-	
     public static void main( String[] args ) {
-		        
+		   
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println( "Please input 1 for Fahrenheit to Celsius, 2 for Celsius to Fahrenheit and 3 to quit.");
@@ -28,17 +18,27 @@ public class ExerciseOne {
 		if ( choice == 1 ) {
 			System.out.println( "What number do you want to convert to Celsius?" );
 			float t = input.nextFloat();
-			float output = fToC();
+			float output = fToC( t );
 			System.out.println( output );
 		} // end choice 1
 		
 		if ( choice == 2 ) {
 			System.out.println( "What number do you want to convert to Fahrenheit?" );
 			float t = input.nextFloat();
-			float output = cToF();
+			float output = cToF( t );
 			System.out.println(cToF( output ));
 		} // end choice 2
 		
 	} // end method main
+	
+	public static float fToC( float t ) {
+		float celsius = (( t - 32 ) * ( 5.0f / 9.0f ));
+		return celsius;
+	} // end method fToC
+	
+	public static float cToF( float t ) {
+		float fahrenheit = ( t * ( 9.0f / 5.0f ) + 32);
+		return fahrenheit;
+	} // end method cToF
 	
 } // end public class
